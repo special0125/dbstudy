e9d5773a80b255aa8f5a0a9baa4ac15a4dcd604d
specail0125 <ykt3401@naver.com>
2021-04-02 14:55:18 +0900

    update-2021-04-02

--- 11_기본SQL작성하기_DDL활용.sql
+++ 11_기본SQL작성하기_DDL활용.sql
@@ -1,20 +1,57 @@
+DROP TABLE customer;
+DROP TABLE bank;
+
 CREATE TABLE bank
 (
-    bank_code VARCHAR2(20), 
+    bank_code VARCHAR2(20),
     bank_name VARCHAR2(30)
 );
-
 CREATE TABLE customer
 (
-    NO NUMBER,
-    NAME VARCHAR2(30) NOT NULL,
+    no NUMBER,
+    name VARCHAR2(30) NOT NULL,
     phone VARCHAR2(30),
     age NUMBER,
-    bank_code VARCHAR2(20) 
+    bank_code VARCHAR2(20)
 );
 
-alter table bank add constraint bank_pk primary key (bank_code);
-alter table customer add constraint customer_pk primary key (no);
-alter table customer add constraint customer_phone_uk unique (phone);
-alter table customer add constraint customer_age_ck check (age between 0 and 100);
-alter table customer add constraint customer_bank_fk foreign key (bank_code) references bank(bank_code); 
+-- 테이블 구조 확인
+DESC bank;
+DESC customer;
+
+-- 테이블 변경
+
+-- 칼럼의 추가
+-- ALTER TABLE 테이블 ADD 칼럼명 타입 [제약조건];
+-- 1. bank 테이블에 bank_phone 칼럼을 추가한다.
+ALTER TABLE bank ADD bank_phone VARCHAR2(15);
+
+-- 칼럼의 수정
+-- ALTER TABLE 테이블 MODIFY 칼럼명 타입 [제약조건];
+-- 1. bank 테이블의 bank_name 칼럼을 varchar2(15)로 수정한다.
+ALTER TABLE bank MODIFY bank_name VARCHAR2(15);
+-- 2. customer 테이블의 age 칼럼을 number(3)로 수정한다.
+ALTER TABLE customer MODIFY age NUMBER(3);
+-- 3. customer 테이블의 phone 칼럼을 NOT NULL로 수정한다.
+ALTER TABLE customer MODIFY phone VARCHAR2(30) NOT NULL;
+-- 4. customer 테이블의 phone 칼럼을 NULL로 수정한다.
+ALTER TABLE customer MODIFY phone VARCHAR2(30) NULL;
+
+-- 칼럼의 삭제
+-- ALTER TABLE 테이블 DROP COLUMN 칼럼명;
+-- 1. bank 테이블의 bank_phone 칼럼을 삭제한다.
+ALTER TABLE bank DROP COLUMN bank_phone;
+
+-- 칼럼의 이름 변경
+-- ALTER TABLE 테이블 RENAME COLUMN 기본칼럼명 TO 신규칼럼명;
+-- 1. customer 테이블의 phone 칼럼명을 contact 으로 수정한다.
+ALTER TABLE customer RENAME COLUMN phone TO contact;
+
+
+-- 제약조건의 추가
+ALTER TABLE bank ADD CONSTRAINT bank_pk PRIMARY KEY(bank_code);
+ALTER TABLE customer ADD CONSTRAINT customer_pk PRIMARY KEY(no);
+ALTER TABLE customer ADD CONSTRAINT customer_phone_uq UNIQUE(phone);
+ALTER TABLE customer ADD CONSTRAINT customer_age_ck CHECK(age BETWEEN 0 AND 100);
+ALTER TABLE customer ADD CONSTRAINT customer_bank_fk FOREIGN KEY(bank_code) REFERENCES bank(bank_code);
+
--- 11_기본SQL작성하기_DDL활용_연습문제.sql
+++ 11_기본SQL작성하기_DDL활용_연습문제.sql
@@ -1,40 +1,88 @@
 -- 1. 국가(nation) 테이블
-create table nation
+CREATE TABLE nation
 (  
-    nation_code number(3),
-    nation_name varchar2(30),
-    nation_prev_rank number,
-    nation_curr_rank number,
-    nation_parti_person number,
-    nation_parti_event number
+    nation_code NUMBER(3),
+    nation_name VARCHAR2(30),
+    nation_prev_rank NUMBER,
+    nation_curr_rank NUMBER,
+    nation_parti_person NUMBER,
+    nation_parti_event NUMBER
 );
 
 -- 2. 종목(event) 테이블
-create table event
+CREATE TABLE event
 (
-    event_code number(5),
-    event_name varchar2(30),
-    event_info varchar2(1000),
-    event_first_year number(4)
+    event_code NUMBER(5),
+    event_name VARCHAR2(30),
+    event_info VARCHAR2(1000),
+    event_first_year NUMBER(4)
 );
 
 -- 3. 선수(player) 테이블
-create table player
+CREATE TABLE player
 (
-    player_code number(5),
-    nation_code number(3),
-    event_code number(5),
-    player_name varchar2(30),
-    player_age number(3),
-    player_rank number
+    player_code NUMBER(5),
+    nation_code NUMBER(3),
+    event_code NUMBER(5),
+    player_name VARCHAR2(30),
+    player_age NUMBER(3),
+    player_rank NUMBER
 );
 
 -- 4. 일정(schedule) 테이블
-create table schedule
+CREATE TABLE schedule
 (
-    nation_code number(3),
-    event_code number(5),
-    schedule_info varchar2(1000),
-    schedule_begin date,
-    schedule_end date
+    nation_code NUMBER(3),
+    event_code NUMBER(5),
+    schedule_info VARCHAR2(1000),
+    schedule_begin DATE,
+    schedule_end DATE
 );
+
+-- 각 테이블에 기본키 추가하기
+ALTER TABLE nation ADD CONSTRAINT nation_pk PRIMARY KEY(nation_code);
+ALTER TABLE event ADD CONSTRAINT event_pk PRIMARY KEY(event_code);
+ALTER TABLE player ADD CONSTRAINT player_pk PRIMARY KEY(player_code);
+ALTER TABLE schedule ADD CONSTRAINT schedule_pk PRIMARY KEY(nation_code, event_code);
+
+-- 선수(player) 테이블에 왜래키 추가하기
+ALTER TABLE player ADD CONSTRAINT player_nation_fk FOREIGN KEY(nation_code) REFERENCES nation(nation_code);
+ALTER TABLE player ADD CONSTRAINT player_event_fk FOREIGN KEY(event_code) REFERENCES event(event_code);
+
+-- 일정(schedule) 테이블에 외래키 추가하기
+ALTER TABLE schedule ADD CONSTRAINT schedule_nation_fk FOREIGN KEY(nation_code) REFERENCES nation(nation_code);
+ALTER TABLE schedule ADD CONSTRAINT schedule_event_fk FOREIGN KEY(event_code) REFERENCES event(event_code);
+
+-- 제약조건의 삭제
+-- ALTER TABLE 테이블 DROP CONSTRAINT 제약조건이름;
+ALTER TABLE player DROP CONSTRAINT player_nation_fk;
+ALTER TABLE schedule DROP CONSTRAINT schedule_nation_fk; 
+ALTER TABLE nation DROP CONSTRAINT nation_pk;  -- nation_pk를 참조하는 외래키를 먼저 지웁니다.
+
+ALTER TABLE player DROP CONSTRAINT player_event_fk;
+ALTER TABLE schedule DROP CONSTRAINT schedule_event_fk;
+ALTER TABLE event DROP CONSTRAINT event_pk;
+
+ALTER TABLE player DROP CONSTRAINT player_pk;
+
+ALTER TABLE schedule DROP CONSTRAINT schedule_pk;
+
+-- 제약조건의 확인
+-- 제약조건을 저장하고 있는 DD(Data Dictionary) : USER_CONSTRAINTS 테이블
+DESC user_constraints;
+SELECT constraint_name, table_name FROM user_constraints WHERE table_name = 'PLAYER';
+
+-- 제약조건의 비활성화
+alter table player disable constraint player_nation_fk;
+
+-- 제약조건의 활성화
+alter table player enable constraint player_nation_fk;
+
+
+
+
+
+
+
+
+
